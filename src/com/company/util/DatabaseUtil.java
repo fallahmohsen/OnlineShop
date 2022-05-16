48cb77522c49e2b11a23eb2a9b95b1a21746f9a6
mohsenfallah <mohsefalllah@gmail.com>
2022-05-16 17:07:19 +0430

    add class  DatabaseUtil

---
+++ src/com/company/util/DatabaseUtil.java
@@ -0,0 +1,33 @@
+package com.company.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+
+    private Connection connection;
+
+    private String databaseUrl;
+
+    private String databaseUser;
+
+    private String databasePassword;
+
+    public DatabaseUtil() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        this.databaseUrl = "jdbc:mysql://localhost:3306/shop";
+        this.databaseUser = "root";
+        this.databasePassword = "20557211";
+
+        this.connection =
+                DriverManager.getConnection(databaseUrl,databaseUser,databasePassword);
+
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
+
+
