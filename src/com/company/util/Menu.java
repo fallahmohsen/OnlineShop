e7fdbc17c8dd23573014e56fe52bc9b3cdbf854a
mohsenfallah <mohsefalllah@gmail.com>
2022-05-16 17:06:02 +0430

    add class util show first menu

---
+++ src/com/company/util/Menu.java
@@ -0,0 +1,12 @@
+package com.company.util;
+
+public class Menu {
+    public void showFirstMenu(){
+        System.out.println("welcome to online shop");
+        System.out.println("enter your number");
+        System.out.println("1.login");
+        System.out.println("2.sign up");
+        System.out.println("3.exit");
+
+    }
+}
